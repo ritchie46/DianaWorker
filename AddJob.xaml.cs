a42bbc540b4876fe4b2963b90d5c6af845f8f1e0
vik <ritchie46@gmail.com>
2016-10-13 20:28:44 +0200

    job list refreshes

--- AddJob.xaml.cs
+++ AddJob.xaml.cs
@@ -21,24 +21,31 @@ namespace ServerWorker
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {
+        private bool _jobs_running;
+        public bool jobs_running
+        {
+            set
+            {
+                _jobs_running = value;
+            }
+            get
+            {
+                return _jobs_running;
+                
+            }
+        }
+
+        public bool path_chosen = false; 
+        public List<string> wait_list = new List<string> { };
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            jobs_running = false;
         }
 
-        public Label Path_dat
-        {
-            get
-            {
-                return this.path_dat;
-            }
-
-            set
-            {
-                this.path_dat = value;
-            }
-        }
 
         private void add_path(object sender, RoutedEventArgs e)
         {
@@ -50,7 +57,8 @@ namespace ServerWorker
                         
             if (succes == true)
             {
-
+                path_chosen = true;
+                
                 // Set the value to the path_dat label
                 path_dat.Content = dialog.FileName;
             }
@@ -59,10 +67,69 @@ namespace ServerWorker
         private async void add_job(object sender, RoutedEventArgs e)
         {
 
-            Debug.WriteLine("button clicked");
+            if (!path_chosen)
+            {
+                MessageBox.Show("The path to your .dat file is not given");
+            }
+            else
+            {
+                // Append path to job list.
+                //job_list.AppendText("\n" + path_dat.ToString());
 
-            string outp = await AsyncDia.add_job_(path_dat);
-            output_box.AppendText(outp);
+                // Add job to wait list. This makes sure the correct jobs are plotted
+                wait_list.Add("\n" + path_dat.Content.ToString());
+
+                // Refresh job list
+                job_list.Document.Blocks.Clear();
+                foreach (string path in wait_list)
+                {
+                    job_list.AppendText(path);
+                }
+                job_list.ScrollToEnd();
+
+                AsyncDia.jobs.Add(path_dat);
+
+                if (!jobs_running)
+                {
+                    output_box.AppendText("\nStarting first job.");
+                    jobs_running = true;
+                    await first_call();
+                    output_box.AppendText("\nAll jobs finished");
+                    jobs_running = false;
+                }
+                //string outp = await AsyncDia.add_job_(path_dat);
+                //output_box.AppendText(outp);
+            }
+        }
+
+        private async Task first_call()
+        {
+            int count = 0;
+
+            while (count < AsyncDia.jobs.Count)
+            {
+                output_box.AppendText("\nStarting " + path_dat.Content.ToString());
+                string outp = await AsyncDia.add_job_(AsyncDia.jobs[count]);
+                output_box.AppendText("\n" + outp);
+
+                // remove path from tasklist
+                wait_list.RemoveAt(0);
+
+                // Refresh job list
+                job_list.Document.Blocks.Clear();
+                foreach (string path in wait_list)
+                {
+                    job_list.AppendText(path);
+                }
+                job_list.ScrollToEnd();
+
+                count++;
+            }
+        }
+
+        private void job_list_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 
@@ -71,10 +138,12 @@ namespace ServerWorker
 
 public class AsyncDia
 {
-    
+    // A list with paths to .dat files
+    public static List<Label> jobs = new List<Label> { };
+
+
     public static async Task<string> add_job_(Label path)
     {
-        Debug.WriteLine("start async");
         var outp = await start_process(path);
         return outp;
 
@@ -83,7 +152,6 @@ public class AsyncDia
     public static async Task<string> start_process(Label path)
     {
 
-        //System.Threading.Thread.Sleep(10000);
         var path_dat = path;
 
         string root = System.IO.Directory.GetParent(path_dat.Content.ToString()).ToString();
